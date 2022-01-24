080a8b30491598f44634517291cd80340750922d
Narendra404 <npaul7345@gmail.com>
2022-01-24 14:04:48 +0000

    Add a button and entry field with bg, fg, width and height

--- script.py
+++ script.py
@@ -1,5 +1,25 @@
 import tkinter as tk
 window = tk.Tk()
-greeting = tk.Label(text="Hello")
+greeting = tk.Label(
+        text="Hello", #text to display
+        foreground="white", #set text color to white 
+        background="black", #set background color to black
+        width=10, #set width of the area
+        height=10 #set height of the area
+        )
+button = tk.Button(
+        text="Click Me",
+        foreground="white",
+        background="blue",
+        width=10,
+        height=5
+        )
+entry = tk.Entry(
+        fg="yellow",
+        bg="blue",
+        width=50
+        )
 greeting.pack()
+button.pack()
+entry.pack()
 window.mainloop()
